a7ae8dd34d71876a97a4bc757b1bd517d7486c8c
pawani mekhala <pawanimekhala3@gmail.com>
2024-06-01 21:45:54 +0530

    navbar

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+// import { footerLinks } from "../utils/data";
+// import { Link } from "react-router-dom";
+// import TextInput from "./TextInput";
+// import CustomButton from "./CustomButton";
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
